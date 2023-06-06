402003d4d7b857eb64d4fe965cf845d892961d0c
vboxuser <vboxuser@ubuntu-linux.myguest.virtualbox.org>
2023-06-06 23:09:19 +0100

    test: write test for account insertion into db

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_"github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbServer = "postgres://root:admin@localhost:5432/bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbServer)
+
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
---
+++ util/random.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charSet = "abcdefghijklmnopqrstuvwxyz"
+
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max - min + 1)
+}
+
+
+func RandomString(n int) string {
+	// Create a byte slice of length n
+	randomBytes := make([]byte, n)
+
+	// Fill the byte slice with random characters from the character set
+	for i := 0; i < n; i++ {
+		randomBytes[i] = charSet[rand.Intn(len(charSet))]
+	}
+
+	// Convert the byte slice to a string and return it
+	return string(randomBytes)
+}
+
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0,10000)
+}
+
+func RandomCurrency() string{
+	currencies := []string {"USD","CAD","EUR"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
